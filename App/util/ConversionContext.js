4577ce71a25060f536e603b282a30439a6f3e68f
Waqar Ahmad <ahmadwaqar@outlook.com>
2022-02-14 19:30:08 +0500

    Add rates from local mimic API

--- App/util/ConversionContext.js
+++ App/util/ConversionContext.js
@@ -1,11 +1,22 @@
 import React, {createContext, useState} from 'react';
+import {api} from './api';
 
 export const ConversionContext = createContext();
 
 const ConversionContextProvider = ({children}) => {
-  const [baseCurrency, setBaseCurrency] = useState('USD');
+  const [baseCurrency, _setBaseCurrency] = useState('USD');
   const [targetCurrency, setTargetCurrency] = useState('PKR');
   const [conversionRate, setConversionRate] = useState(171.5);
+
+  const setBaseCurrency = currency => {
+    api(`/latest?base=${currency}`).then(res => {
+      const {rates} = res;
+      const rate = rates[baseCurrency];
+      setConversionRate(rate);
+    });
+
+    _setBaseCurrency(currency);
+  };
 
   const swapCurrencies = () => {
     setBaseCurrency(targetCurrency);
